4b55303e756aa151c6acd77b0e444ef1a23c3e12
dylancoffey99 <dylancoffey99@gmail.com>
2021-10-29 18:03:16 +0100

    Implemented the get_all_rows method, and the closing of csv files after no longer being used

--- db_handler.py
+++ db_handler.py
@@ -15,48 +15,66 @@ class ProductDB(db.AbstractDBHandler):
         return database
 
     def read_db(self, operation):
-        reader = csv.reader(self.open_db(operation), delimiter=",")
-        return reader
+        database = self.open_db(operation)
+        reader = csv.reader(database, delimiter=",")
+        return [reader, database]
 
     def write_db(self, operation):
-        writer = csv.writer(self.open_db(operation), delimiter=",")
-        return writer
+        database = self.open_db(operation)
+        writer = csv.writer(database, delimiter=",")
+        return [writer, database]
 
     def add_row(self, product):
         writer = self.write_db("a")
-        writer.writerow(product)
+        writer[0].writerow(product)
+        writer[1].close()
 
     def get_row(self, product_id):
         reader = self.read_db("r")
-        for row in reader:
+        for row in reader[0]:
             if row[0] == product_id:
+                reader[1].close()
                 return row
+
+    def get_all_rows(self):
+        temp_rows = []
+        reader = self.read_db("r")
+        next(reader[0])
+        for row in reader[0]:
+            temp_rows.append(row)
+        reader[1].close()
+        return temp_rows
 
     def get_id(self, product_name):
         reader = self.read_db("r")
-        for row in reader:
+        for row in reader[0]:
             if row[1] == product_name:
+                reader[1].close()
                 return row[0]
 
     def remove_row(self, product_id):
-        temp_rows = list()
+        temp_rows = []
         reader = self.read_db("r")
-        for row in reader:
+        for row in reader[0]:
             temp_rows.append(row)
             if row[0] == product_id:
                 temp_rows.remove(row)
         writer = self.write_db("w")
-        writer.writerows(temp_rows)
+        writer[0].writerows(temp_rows)
+        reader[1].close()
+        writer[1].close()
 
     def edit_row(self, product_id, column, new_value):
-        temp_rows = list()
+        temp_rows = []
         reader = self.read_db("r")
-        for row in reader:
+        for row in reader[0]:
             if row[0] == product_id:
                 row[column] = new_value
             temp_rows.append(row)
         writer = self.write_db("w")
-        writer.writerows(temp_rows)
+        writer[0].writerows(temp_rows)
+        reader[1].close()
+        writer[1].close()
 
 
 class UserDB(db.AbstractDBHandler):
@@ -68,25 +86,39 @@ class UserDB(db.AbstractDBHandler):
         return database
 
     def read_db(self, operation):
-        reader = csv.reader(self.open_db(operation), delimiter=",")
-        return reader
+        database = self.open_db(operation)
+        reader = csv.reader(database, delimiter=",")
+        return [reader, database]
 
     def write_db(self, operation):
-        writer = csv.writer(self.open_db(operation), delimiter=",")
-        return writer
+        database = self.open_db(operation)
+        writer = csv.writer(database, delimiter=",")
+        return [writer, database]
 
     def add_row(self, user):
         writer = self.write_db("a")
-        writer.writerow(user)
+        writer[0].writerow(user)
+        writer[1].close()
 
     def get_row(self, user_id):
         reader = self.read_db("r")
-        for row in reader:
+        for row in reader[0]:
             if row[0] == user_id:
+                reader[1].close()
                 return row
+
+    def get_all_rows(self):
+        temp_rows = []
+        reader = self.read_db("r")
+        next(reader[0])
+        for row in reader[0]:
+            temp_rows.append(row)
+        reader[1].close()
+        return temp_rows
 
     def get_id(self, user_name):
         reader = self.read_db("r")
-        for row in reader:
+        for row in reader[0]:
             if row[1] == user_name:
+                reader[1].close()
                 return row[0]
