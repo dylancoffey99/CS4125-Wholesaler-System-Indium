052a16b623c165f4c5407268e06f679056e4f785
dylancoffey99 <dylancoffey99@gmail.com>
2021-10-29 18:55:16 +0100

    Implemented better functionality for closing csv files after no longer being used

--- db_handler.py
+++ db_handler.py
@@ -9,116 +9,114 @@ import abstract_db_handler as db
 class ProductDB(db.AbstractDBHandler):
     def __init__(self, db_name):
         self._db_name = db_name
+        self._db = None
 
     def open_db(self, operation):
-        database = open(self._db_name + ".csv", operation, newline="")
-        return database
+        self._db = open(self._db_name + ".csv", operation, newline="")
 
     def read_db(self, operation):
-        database = self.open_db(operation)
-        reader = csv.reader(database, delimiter=",")
-        return [reader, database]
+        self.open_db(operation)
+        reader = csv.reader(self._db, delimiter=",")
+        return reader
 
     def write_db(self, operation):
-        database = self.open_db(operation)
-        writer = csv.writer(database, delimiter=",")
-        return [writer, database]
+        self.open_db(operation)
+        writer = csv.writer(self._db, delimiter=",")
+        return writer
 
     def add_row(self, product):
         writer = self.write_db("a")
-        writer[0].writerow(product)
-        writer[1].close()
+        writer.writerow(product)
+        self._db.close()
 
     def get_row(self, product_id):
         reader = self.read_db("r")
-        for row in reader[0]:
+        for row in reader:
             if row[0] == product_id:
-                reader[1].close()
+                self._db.close()
                 return row
 
     def get_all_rows(self):
         temp_rows = []
         reader = self.read_db("r")
-        next(reader[0])
-        for row in reader[0]:
+        next(reader)
+        for row in reader:
             temp_rows.append(row)
-        reader[1].close()
+        self._db.close()
         return temp_rows
 
     def get_id(self, product_name):
         reader = self.read_db("r")
-        for row in reader[0]:
+        for row in reader:
             if row[1] == product_name:
-                reader[1].close()
+                self._db.close()
                 return row[0]
 
     def remove_row(self, product_id):
         temp_rows = []
         reader = self.read_db("r")
-        for row in reader[0]:
+        for row in reader:
             temp_rows.append(row)
             if row[0] == product_id:
                 temp_rows.remove(row)
         writer = self.write_db("w")
-        writer[0].writerows(temp_rows)
-        reader[1].close()
-        writer[1].close()
+        writer.writerows(temp_rows)
+        self._db.close()
 
     def edit_row(self, product_id, column, new_value):
         temp_rows = []
         reader = self.read_db("r")
-        for row in reader[0]:
+        for row in reader:
             if row[0] == product_id:
                 row[column] = new_value
             temp_rows.append(row)
         writer = self.write_db("w")
-        writer[0].writerows(temp_rows)
-        reader[1].close()
-        writer[1].close()
+        writer.writerows(temp_rows)
+        self._db.close()
 
 
 class UserDB(db.AbstractDBHandler):
     def __init__(self, db_name):
         self._db_name = db_name
+        self._db = None
 
     def open_db(self, operation):
-        database = open(self._db_name + ".csv", operation, newline="")
-        return database
+        self._db = open(self._db_name + ".csv", operation, newline="")
 
     def read_db(self, operation):
-        database = self.open_db(operation)
-        reader = csv.reader(database, delimiter=",")
-        return [reader, database]
+        self.open_db(operation)
+        reader = csv.reader(self._db, delimiter=",")
+        return reader
 
     def write_db(self, operation):
-        database = self.open_db(operation)
-        writer = csv.writer(database, delimiter=",")
-        return [writer, database]
+        self.open_db(operation)
+        writer = csv.writer(self._db, delimiter=",")
+        return writer
 
     def add_row(self, user):
         writer = self.write_db("a")
-        writer[0].writerow(user)
-        writer[1].close()
+        writer.writerow(user)
+        self._db.close()
 
     def get_row(self, user_id):
         reader = self.read_db("r")
-        for row in reader[0]:
+        for row in reader:
             if row[0] == user_id:
-                reader[1].close()
+                self._db.close()
                 return row
 
     def get_all_rows(self):
         temp_rows = []
         reader = self.read_db("r")
-        next(reader[0])
-        for row in reader[0]:
+        next(reader)
+        for row in reader:
             temp_rows.append(row)
-        reader[1].close()
+        self._db.close()
         return temp_rows
 
     def get_id(self, user_name):
         reader = self.read_db("r")
-        for row in reader[0]:
+        for row in reader:
             if row[1] == user_name:
-                reader[1].close()
+                self._db.close()
                 return row[0]
