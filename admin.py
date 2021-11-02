45817e3477bb26db9c805a407eee2cf02725b061
wasimghazal <wasimghazal@hotmail.com>
2021-11-02 21:47:38 +0000

    Add functionality to the class

--- admin.py
+++ admin.py
@@ -1,31 +1,21 @@
-# admin.py
-############################
-# Implemented class for the Admin
-#############################
-# Author: Wasim - 17193559
-# Last edited on 02/11
-# Modification time 07:00 PM
-
 from user import User
 from stock_management import Product
 from db_handler import ProductDB
 
 
 class Admin:
-    #def __init__():
-    def __init__(self, product_id: int, product_name: str):
-        self._product_id = product_id
-        self._product_name = product_name
+    def __init__(self, _user: User):
+        self._user_id = _user.get_user_id()
+        self._user_name = _user.get_user_name()
+        self._is_admin = _user.get_is_admin()
+        self._db = ProductDB("productDB")
 
-    # return void
     def add_product(self, product: Product):
-        # We need to write those products to the CSV file
+        self._db.add_product(product)
 
-    # return void
-    def remove_product(self):
-        # We need to remove those products to the CSV file
+    def remove_product(self, product: Product):
+        self._db.remove_product(product)
 
     # return list
     def get_orders(self):
-        # print the products order
-
+        pass  # print the products order
