05c9cc2b094924b9bfc4d37474f48d6d8a8f8aa7
dylancoffey99 <dylancoffey99@gmail.com>
2021-11-03 16:56:09 +0000

    Implemented UserDB and its methods

--- system/models/database/db_handler.py
+++ system/models/database/db_handler.py
@@ -1,6 +1,8 @@
 import csv
 from typing import List
+from system.models.users.user import User
 from system.models.shopping.product import Product
+from system.models.database.abstract_db_handler import AbstractUserDB
 from system.models.database.abstract_db_handler import AbstractProductDB
 
 
@@ -71,3 +73,34 @@ class ProductDB(AbstractProductDB):
                 product = Product(int(row[0]), row[1], int(row[2]), float(row[3]))
                 products.append(product)
             return products
+
+
+class UserDB(AbstractUserDB):
+    def __init__(self, db_name: str):
+        self._db_name = db_name
+        self.csv = None
+        self.reader = None
+        self.writer = None
+
+    def add_user(self, user: User):
+        with open(self._db_name + ".csv", "a", newline="", encoding="utf-8") as self.csv:
+            self.writer = csv.writer(self.csv, delimiter=",")
+            self.writer.writerow(user.get_user_as_list())
+
+    def get_user(self, user_id: int) -> User:
+        with open(self._db_name + ".csv", "r", newline="", encoding="utf-8") as self.csv:
+            self.reader = csv.reader(self.csv, delimiter=",")
+            for row in self.reader:
+                if row[0] == str(user_id):
+                    user = User(int(row[0]), row[1], row[2], bool(row[3]))
+                return user
+
+    def get_all_users(self) -> List[User]:
+        with open(self._db_name + ".csv", "r", newline="", encoding="utf-8") as self.csv:
+            self.reader = csv.reader(self.csv, delimiter=",")
+            next(self.reader)
+            users = []
+            for row in self.reader:
+                user = User(int(row[0]), row[1], row[2], bool(row[3]))
+                users.append(user)
+            return users
