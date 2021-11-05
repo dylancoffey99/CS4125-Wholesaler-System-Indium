407c054c02fc6c20f155fe8a985bac0e548035aa
nadinefrawley <18219659@studentmail.ul.ie>
2021-11-05 13:20:20 +0000

    Updated files

--- system/models/authentication/login.py
+++ system/models/authentication/login.py
@@ -1,10 +1,9 @@
+from system.models.database.db_handler import UserDB
+
 class Login:
-    def __init__(self, user_id: int, user_name: str, password: str, is_admin: bool, error: str):
-        self._user_id = user_id
-        self._user_name = user_name
-        self._password = password
-        self._is_admin = is_admin
-        self._error = error
+    def init(self, user: UserDB):
+        self._user = user
+        self._user_db = UserDB("userDB")
 
     def check_user(self, user_name, password):
         print (self._user_name)
--- system/models/authentication/register.py
+++ system/models/authentication/register.py
@@ -1,14 +1,22 @@
-import hashlib
-import os
+from system.models.database.db_handler import UserDB
 
 class Register:
-    def __init__(self, user_id: int, user_name: str, password: str, is_admin: bool, error: str):
+    def __init__(self, user_id: int, user_name: str, password: str, is_admin: bool):
         self._user_id = user_id
         self._user_name = user_name
         self._password = password
         self._is_admin = is_admin
 
-    def ask_user_credentials(self):
+    def add_user(self, user: UserDB):
+        pass
+
+    def get_user(self, user_id: int) -> UserDB:
+        pass
+
+    def get_all_users(self) -> List[UserDB]:
+        pass
+
+    def ask_user_credentials(self): -> return str:
         print("Please Provide")
         user_name = str(input("Name: "))
         password = str(input("Password: "))
