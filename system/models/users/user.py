9344e7c8d9057c0a568c01d92164a52e449c7208
wasimghazal <wasimghazal@hotmail.com>
2021-11-06 20:50:05 +0000

    Removed user_id, set_user() and set_hashed_password() methods, and added country_id

--- system/models/users/user.py
+++ system/models/users/user.py
@@ -20,30 +20,5 @@ class User:
     def get_is_admin(self) -> bool:
         return self._is_admin
 
-    def get_user_as_list(self) -> List[str]:
-        return [str(self._user_id), self._user_name, self._password, str(self._is_admin)]
-
-    def set_user(self, user_name: str, password: str):
-        self._user_name = user_name
-        self._password = self.get_hashed_password(password)
-
-    def get_hashed_password(self, password: str) -> str:
-        # https://nitratine.net/blog/post/how-to-hash-passwords-in-python/
-        # https://stackoverflow.com/questions/48448830/hashing-password-in-py-file
-        salt = os.urandom(32)
-        key = hashlib.pbkdf2_hmac(
-            'sha256',  # The hash digest algorithm for HMAC
-            password.encode('utf-8'),  # Convert the password to bytes
-            salt,  # Provide the salt
-            100000  # It is recommended to use at least 100,000 iterations of SHA-256
-        )
-        # Store them as:
-        storage = salt + key
-
-        # Getting the values back out
-        salt_from_storage = storage[:32]  # 32 is the length of the salt
-        key_from_storage = storage[32:]
-
-        salt_and_key = [salt_from_storage, key_from_storage]
-
-        return salt_and_key
+    def get_country_id(self) -> int:
+        return self._country_id
