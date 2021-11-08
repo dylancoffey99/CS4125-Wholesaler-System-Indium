05ee654cdbc7f6e3db07a63dbdd1f2926436e51e
dylancoffey99 <dylancoffey99@gmail.com>
2021-11-08 01:59:17 +0000

    Removed initialisation of userDB in main python file

--- __main__.py
+++ __main__.py
@@ -1,6 +1,4 @@
 import tkinter as tk
-from system.database.db_handler import UserDB
-from system.database.db_handler import ProductDB
 
 
 class MainApplication(tk.Frame):
@@ -9,8 +7,6 @@ class MainApplication(tk.Frame):
 
 
 if __name__ == "__main__":
-    user_db = UserDB("userDB")
-    product_db = ProductDB("productDB")
     root = tk.Tk()
     MainApplication(root)
     root.mainloop()
