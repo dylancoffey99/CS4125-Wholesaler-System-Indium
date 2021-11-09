741f2b828eb2f62f44672502019117163efbce71
dylancoffey99 <dylancoffey99@gmail.com>
2021-11-09 20:50:47 +0000

    Refactored home_view.py, removed main code and intialised and configured root/controller/frame for HomeView. Centered frame widgets and organised them by their type

--- system/views/home_view.py
+++ system/views/home_view.py
@@ -1,62 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
-import system.views.register_view
-from system.views.login_view import LoginPage
-from system.views.user_view import UserPage
-from system.views.admin_view import AdminPage
 
 
-class View(tk.Tk):
+class HomeView:
+    def __init__(self, root, controller):
+        self.root = root
+        self.root.geometry("600x500")
+        self.root.title("Wholesaler System")
+        self.root.rowconfigure(0, weight=1)
+        self.root.columnconfigure(0, weight=1)
+        self.root.resizable(width=False, height=False)
+        self.controller = controller
+        self.frame = tk.Frame(self.root)
+        self.frame.grid(row=0, column=0)
 
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self,  *args, **kwargs)
+        # Frame labels
+        system_label = ttk.Label(self.frame, text="Wholesaler System", font=(None, 20))
+        system_label.grid(row=0, column=0, pady=10)
 
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}
-        for F in (HomeView, system.views.registration_page.RegistrationPage,
-                  LoginPage, UserPage, AdminPage):
-            frame = F(container, self)
-
-            self.frames[F] = frame
-
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(HomeView)
-
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
-
-
-class HomeView(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Home Page", font='Aerial 20 bold')
-        label.grid(row=0, column=3, padx=10, pady=10)
-
-        register_button = ttk.Button(self, text="Register",
-                                     command=lambda: controller.show_frame
-                                     (system.views.registration_page.RegistrationPage))
-
-        register_button.grid(row=9, column=2, padx=10, pady=10)
-        login_button = ttk.Button(self, text="Login",
-                                  command=lambda: controller.show_frame(LoginPage))
-
-        login_button.grid(row=9, column=3, padx=10, pady=10)
-
-
-def main(self):
-    self.resizable(width=False, height=False)
-    self.geometry('600x500')
-    self.config(bg="grey")
-
-
-if __name__ == '__main__':
-    root = View()
-    main(root)
-    root.mainloop()
+        # Frame buttons
+        login_button = ttk.Button(self.frame, text="Login")
+        login_button.grid(row=1, column=0, pady=10)
+        register_button = ttk.Button(self.frame, text="Register")
+        register_button.grid(row=2, column=0, pady=10)
