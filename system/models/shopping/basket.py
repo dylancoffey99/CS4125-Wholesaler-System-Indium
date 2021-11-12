043d044da56e9386cbd81af316a2b3ae8c8c00d6
nadinefrawley <18219659@studentmail.ul.ie>
2021-11-12 14:28:32 +0000

    Updated basket class

--- system/models/shopping/basket.py
+++ system/models/shopping/basket.py
@@ -3,28 +3,22 @@ from system.models.users.customer import Customer
 from system.models.shopping.product import Product
 
 class Basket:
-    def __init__(self, basket_items: List[Product], customer: Customer, basket_total: int):
+    def __init__(self, basket_items: List[Product], basket_subtotal: int, customer: Customer):
         self._basket_items = basket_items
+        self._basket_subtotal = basket_subtotal
         self._customer = customer
-        self._basket_total = basket_total
-
-    def set_order_id(self) -> int: #this might be done in checkout so remove if wanted
-        return self._order_id
 
     def get_basket(self) -> List[Product]:
         return self._basket_items
 
+    def get_basket_subtotal(self) -> int:
+        return self._basket_subtotal
+
     def get_customer(self) -> Customer:
         return self._customer
 
-    def get_basket_as_list(self) -> List:
-        return [self._order_id, self._basket_items, self._customer]
+    def add_item(self) -> List[Product]:
+        self
 
-    def calc_subtotal(self) -> int:
-        return self._basket_total
-
-    def add_product(self) -> List[Product]:
-        return self._basket_items
-
-    def remove_product(self) -> List[Product]:
-        return self._basket_items
+    def remove_item(self) -> List[Product]:
+        self
