469e2ab739c57e802a1dcf398926f059037df840
dylancoffey99 <dylancoffey99@gmail.com>
2021-11-12 18:33:38 +0000

    Implemented add_item/remove_item methods, changed get_basket() to get_basket_items()

--- system/models/shopping/basket.py
+++ system/models/shopping/basket.py
@@ -2,13 +2,14 @@ from typing import List
 from system.models.users.customer import Customer
 from system.models.shopping.product import Product
 
+
 class Basket:
     def __init__(self, basket_items: List[Product], basket_subtotal: int, customer: Customer):
         self._basket_items = basket_items
         self._basket_subtotal = basket_subtotal
         self._customer = customer
 
-    def get_basket(self) -> List[Product]:
+    def get_basket_items(self) -> List[Product]:
         return self._basket_items
 
     def get_basket_subtotal(self) -> int:
@@ -17,8 +18,8 @@ class Basket:
     def get_customer(self) -> Customer:
         return self._customer
 
-    def add_item(self) -> List[Product]:
-        self
+    def add_item(self, product: Product):
+        self._basket_items.append(product)
 
-    def remove_item(self) -> List[Product]:
-        self
+    def remove_item(self, product: Product):
+        self._basket_items.remove(product)
