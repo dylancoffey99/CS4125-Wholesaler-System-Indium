1b4e6d01923e2a9aedc43802e4322bc7ae0cf70b
dylancoffey99 <dylancoffey99@gmail.com>
2021-11-13 19:25:11 +0000

    Implemented creation of mock_db's, mock_product, and mock_user. Created unittest setUp() and tearDown() methods

--- tests/test_db_handler.py
+++ tests/test_db_handler.py
@@ -1,12 +1,27 @@
+import os
+import csv
+from unittest import TestCase
 from system.models.users.user import User
 from system.models.shopping.product import Product
 from system.database.db_handler import UserDB
 from system.database.db_handler import ProductDB
 
 
-class TestProductDB:
-    mock_db = ProductDB("testProductDB")
-    mock_product = Product("product", 100, 9.99)
+class TestProductDB(TestCase):
+    mock_db_name = "testProductDB"
+    mock_db = ProductDB(mock_db_name)
+    mock_product = Product("mock_product", 75, 100)
+
+    def setUp(self):
+        with open(self.mock_db_name + ".csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(["product_name", "product_quantity", "product_price"])
+            writer.writerow(["product1", "25", "50"])
+            writer.writerow(["product2", "50", "75"])
+            writer.writerow([""])
+
+    def tearDown(self):
+        os.remove(self.mock_db_name)
 
     def test_add_product(self):
         pass
@@ -30,9 +45,21 @@ class TestProductDB:
         pass
 
 
-class TestUserDB:
-    mock_db = UserDB("testUserDB")
-    mock_user = User("username", "password", 0, 1)
+class TestUserDB(TestCase):
+    mock_db_name = "testUserDB"
+    mock_db = UserDB(mock_db_name)
+    mock_user = User("mock_user", "password", 0, 1)
+
+    def setUp(self):
+        with open(self.mock_db_name + ".csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(["username", "password", "is_admin", "country_id"])
+            writer.writerow(["user1", "password1", "0", "1"])
+            writer.writerow(["user2", "password2", "0", "2"])
+            writer.writerow([""])
+
+    def tearDown(self):
+        os.remove(self.mock_db_name)
 
     def test_add_user(self):
         pass
