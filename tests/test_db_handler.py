bca68d91da68d031aa2f89024dc93cf805c8b55d
dylancoffey99 <dylancoffey99@gmail.com>
2021-11-13 18:22:26 +0000

    Created TestProductDB class, initialised mock_db and mock_product, added test methods

--- tests/test_db_handler.py
+++ tests/test_db_handler.py
@@ -1,5 +1,33 @@
 from system.models.users.user import User
+from system.models.shopping.product import Product
 from system.database.db_handler import UserDB
+from system.database.db_handler import ProductDB
+
+
+class TestProductDB:
+    mock_db = ProductDB("testProductDB")
+    mock_product = Product("product", 100, 9.99)
+
+    def test_add_product(self):
+        pass
+
+    def test_remove_product(self):
+        pass
+
+    def test_edit_product(self):
+        pass
+
+    def sub_product_quantity(self):
+        pass
+
+    def test_get_product(self):
+        pass
+
+    def test_get_all_products(self):
+        pass
+
+    def test_product_exists(self):
+        pass
 
 
 class TestUserDB:
