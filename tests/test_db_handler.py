a811054edaab47c42ccb96e3655dfc216c937419
dylancoffey99 <dylancoffey99@gmail.com>
2021-11-13 20:02:40 +0000

    Initialised mock_db products/users first, then used get methods instead. Implemented test_add_product/test_add_user methods

--- tests/test_db_handler.py
+++ tests/test_db_handler.py
@@ -10,21 +10,27 @@ from system.database.db_handler import ProductDB
 class TestProductDB(TestCase):
     mock_db_name = "testProductDB"
     mock_db = ProductDB(mock_db_name)
-    mock_product = Product("mock_product", 75, 100)
+    mock_db_product1 = Product("product1", 25, 50)
+    mock_db_product2 = Product("product2", 50, 75)
 
     def setUp(self):
         with open(self.mock_db_name + ".csv", "w", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
             writer.writerow(["product_name", "product_quantity", "product_price"])
-            writer.writerow(["product1", "25", "50"])
-            writer.writerow(["product2", "50", "75"])
+            writer.writerow([self.mock_db_product1.get_product_name(),
+                             self.mock_db_product1.get_product_quantity(),
+                             self.mock_db_product1.get_product_price()])
+            writer.writerow([self.mock_db_product2.get_product_name(),
+                             self.mock_db_product2.get_product_quantity(),
+                             self.mock_db_product2.get_product_price()])
             writer.writerow([""])
 
     def tearDown(self):
         os.remove(self.mock_db_name)
 
     def test_add_product(self):
-        pass
+        new_product = Product("new_product", 75, 100)
+        self.assertEqual(self.mock_db.add_product(new_product), True)
 
     def test_remove_product(self):
         pass
@@ -48,21 +54,29 @@ class TestProductDB(TestCase):
 class TestUserDB(TestCase):
     mock_db_name = "testUserDB"
     mock_db = UserDB(mock_db_name)
-    mock_user = User("mock_user", "password", 0, 1)
+    mock_db_user1 = User("user1", "password1", 0, 1)
+    mock_db_user2 = User("user2", "password2", 0, 2)
 
     def setUp(self):
         with open(self.mock_db_name + ".csv", "w", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
             writer.writerow(["username", "password", "is_admin", "country_id"])
-            writer.writerow(["user1", "password1", "0", "1"])
-            writer.writerow(["user2", "password2", "0", "2"])
+            writer.writerow([self.mock_db_user1.get_user_name(),
+                             self.mock_db_user1.get_password(),
+                             self.mock_db_user1.get_is_admin(),
+                             self.mock_db_user1.get_country_id()])
+            writer.writerow([self.mock_db_user2.get_user_name(),
+                             self.mock_db_user2.get_password(),
+                             self.mock_db_user2.get_is_admin(),
+                             self.mock_db_user2.get_country_id()])
             writer.writerow([""])
 
     def tearDown(self):
         os.remove(self.mock_db_name)
 
     def test_add_user(self):
-        pass
+        new_user = User("new_user", "password", 0, 1)
+        self.assertEqual(self.mock_db.add_user(new_user), True)
 
     def test_get_user(self):
         pass
