97cad63f4a48db72aff8b9c22af15b7f01ae0748
wasimghazal <wasimghazal@hotmail.com>
2021-11-17 17:15:33 +0000

    Fix widget row numbers, added commands to buttons, entry and combobox

--- system/views/customer_view.py
+++ system/views/customer_view.py
@@ -14,18 +14,19 @@ class CustomerView:
 
         # Frame labels
         product_label = ttk.Label(self.root, text="Choose a product")
-        product_label.grid(row=1, column=0, padx=10, pady=5)
+        product_label.grid(row=0, column=0, padx=10, pady=10)
         product_quantity_label = ttk.Label(self.root, text="Quantity")
-        product_quantity_label.grid(row=1, column=2, padx=10, pady=10)
+        product_quantity_label.grid(row=0, column=2, padx=10, pady=10)
 
         # Frame combobox/entries
         product_list = tk.StringVar()
         product_combobox = ttk.Combobox(self.root, width=34, state="readonly",
-                                        textvariable=product_list)
-        product_combobox["values"] = ("Laptop", "Monitors", "Mouse", "Keyboard")
-        product_combobox.grid(row=1, column=1, pady=10)
-        product_quantity_entry = ttk.Entry(self.root, width=18)
-        product_quantity_entry.grid(row=1, column=3, padx=10, pady=10)
+                                        textvariable=self.controller.input["product_name"])
+        product_combobox["values"] = self.controller.fill_products()
+        product_combobox.grid(row=0, column=1, pady=10)
+        product_quantity_entry = ttk.Entry(self.root, width=18,
+                                           textvariable=self.controller.input["product_quantity"])
+        product_quantity_entry.grid(row=0, column=3, padx=10, pady=10)
 
         # Frame tree views
         product_tree_view = ttk.Treeview(self.root, column=("c1", "c2", "c3"),
@@ -36,15 +37,16 @@ class CustomerView:
         product_tree_view.heading("c1", text="Product Name")
         product_tree_view.heading("c2", text="Quantity")
         product_tree_view.heading("c3", text="Price")
-        product_tree_view.grid(row=2, rowspan=5, column=0, columnspan=2, padx=10, pady=2)
+        product_tree_view.grid(row=1, rowspan=5, column=0, columnspan=2, padx=10, pady=2)
 
         # Frame buttons
-        add_product_button = ttk.Button(self.root, width=20, text="Add to Basket")
-        add_product_button.grid(row=2, column=2, columnspan=3, padx=10)
+        add_product_button = ttk.Button(self.root, width=20, text="Add to Basket", command=lambda:
+                                        self.controller.add_product(product_tree_view))
+        add_product_button.grid(row=1, column=2, columnspan=3, padx=10)
         remove_product_button = ttk.Button(self.root, width=20, text="Remove from Basket")
-        remove_product_button.grid(row=3, column=2, columnspan=3, padx=10)
+        remove_product_button.grid(row=2, column=2, columnspan=3, padx=10)
         checkout_button = ttk.Button(self.root, width=20, text="Checkout")
-        checkout_button.grid(row=4, column=2, columnspan=3, padx=10)
+        checkout_button.grid(row=3, column=2, columnspan=3, padx=10)
         log_out_button = ttk.Button(self.root, width=20, text="Logout", command=lambda:
                                     self.controller.logout_user(self.root, self.frame))
-        log_out_button.grid(row=6, column=2, columnspan=3, padx=10)
+        log_out_button.grid(row=5, column=2, columnspan=3, padx=10)
