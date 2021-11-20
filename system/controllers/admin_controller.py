21f9cd046b56b84d73f5ae81795a41f3565cf917
Miracle2001 <47611584+Miracle2001@users.noreply.github.com>
2021-11-20 22:24:13 +0000

    Implemented view_order and insert_order, changed logout_admin to logout_user

--- system/controllers/admin_controller.py
+++ system/controllers/admin_controller.py
@@ -9,9 +9,9 @@ from system.controllers.abstract_controllers import AbstractAdminController
 
 
 class AdminController(AbstractAdminController):
-    def __init__(self, root, user, access_controller):
-        self.root = root
-        self.user = user
+    def __init__(self, access_controller):
+        self.root = access_controller.root
+        self.user = access_controller.user
         self.access_controller = access_controller
         self.user_db = UserDB("system/database/userDB")
         self.order_db = OrderDB("system/database/orderDB")
@@ -24,7 +24,7 @@ class AdminController(AbstractAdminController):
         users = self.user_db.get_all_users()
         user_names = []
         for user, _ in enumerate(users):
-            if users[user].get_is_admin() != 1:
+            if users[user].get_is_admin() == 0:
                 user_names.append(users[user].get_user_name())
         return user_names
 
