7196b04664643641d74a3c466ee8ab86ea7a1a41
Miracle2001 <47611584+Miracle2001@users.noreply.github.com>
2021-11-20 22:24:04 +0000

    Implemented view_order and insert_order, changed logout_admin to logout_user

--- system/controllers/admin_controller.py
+++ system/controllers/admin_controller.py
@@ -1,6 +1,9 @@
 import tkinter as tk
+from tkinter import ttk
+from typing import List
 from system import views
 from system.database.db_handler import UserDB
+from system.database.db_handler import OrderDB
 from system.database.db_handler import ProductDB
 from system.controllers.abstract_controllers import AbstractAdminController
 
@@ -11,9 +14,11 @@ class AdminController(AbstractAdminController):
         self.user = user
         self.access_controller = access_controller
         self.user_db = UserDB("system/database/userDB")
+        self.order_db = OrderDB("system/database/orderDB")
         self.product_db = ProductDB("system/database/productDB")
+        self.order_input = {"user_name": tk.StringVar(),
+                            "discount_category": tk.StringVar()}
         self.view = views.AdminView(self.root, self)
-        self.input = {}
 
     def fill_users(self):
         users = self.user_db.get_all_users()
@@ -23,7 +28,12 @@ class AdminController(AbstractAdminController):
                 user_names.append(users[user].get_user_name())
         return user_names
 
-    def logout_admin(self, root: tk.Tk, frame: tk.Frame):
+    def view_order(self, tree_view: ttk.Treeview):
+        user_name = self.order_input["user_name"].get()
+        orders = self.order_db.get_customer_orders(user_name)
+        self.insert_order(tree_view, orders)
+
+    def logout_user(self, root: tk.Tk, frame: tk.Frame):
         self.destroy_frame(frame)
         for child in root.winfo_children():
             child.destroy()
@@ -34,3 +44,14 @@ class AdminController(AbstractAdminController):
         for widget in frame.winfo_children():
             widget.destroy()
         frame.destroy()
+
+    @staticmethod
+    def insert_order(tree_view: ttk.Treeview, orders: List):
+        for product in orders:
+            product_name = product[1]
+            date = product[2]
+            subtotal = product[3]
+            if product != orders[-1]:
+                tree_view.insert("", "end", text="Item", values=(product_name, "", ""))
+            else:
+                tree_view.insert("", "end", text="Item", values=(product_name, date, subtotal))
