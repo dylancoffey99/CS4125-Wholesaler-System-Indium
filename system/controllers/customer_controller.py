8db3d0d3cc62d984f8bc0b958411cf3306291542
wasimghazal <wasimghazal@hotmail.com>
2021-11-23 20:29:36 +0000

    Added implementation for add_product() and remove_product()

--- system/controllers/customer_controller.py
+++ system/controllers/customer_controller.py
@@ -15,9 +15,8 @@ class CustomerController(AbstractCustomerController):
         self.input = {"product_name": tk.StringVar(),
                       "product_quantity": tk.StringVar()}
         self.view = views.CustomerView(self.root, self)
-        self.basket = None
+        self.basket = Basket([], 0)
         self.fill_products()
-        self.create_basket()
 
     def fill_products(self):
         products = self.product_db.get_all_products()
@@ -26,19 +25,39 @@ class CustomerController(AbstractCustomerController):
             product_names.append(products[product].get_product_name())
         return product_names
 
-    def create_basket(self):
-        basket_items = []
-        basket_subtotal = 0
-        self.basket = Basket(basket_items, basket_subtotal)
-
     def add_product(self, tree_view: ttk.Treeview):
         product_name = self.input["product_name"].get()
         quantity = self.input["product_quantity"].get()
-        product = self.product_db.get_product(product_name)
-        price = float(quantity) * product.get_product_price()
-        self.basket.add_item(product)
-        self.basket.add_basket_subtotal(price)
-        self.insert_data(tree_view, product_name, quantity, price)
+        if product_name == "":
+            print("Error: please select a product!")
+        elif quantity == "":
+            print("Error: please enter the quantity!")
+        else:
+            product = self.product_db.get_product(product_name)
+            price = float(quantity) * product.get_product_price()
+            self.basket.add_item(product)
+            self.basket.add_basket_subtotal(price)
+            self.insert_item(tree_view, product_name, quantity, price)
+
+    def remove_product(self, tree_view: ttk.Treeview):
+        selected_item = tree_view.focus()
+        if not selected_item:
+            print("Error: please select from the basket items!")
+        else:
+            item_dict = tree_view.item(selected_item)
+            values = list(item_dict.values())
+            product_name = values[2][0]
+            quantity = values[2][1]
+            products = self.basket.get_basket_items()
+            for product in products:
+                if product.get_product_name() == product_name:
+                    price = float(quantity) * product.get_product_price()
+                    self.basket.remove_item(product)
+                    self.basket.sub_basket_subtotal(price)
+                    self.remove_item(tree_view)
+
+    def checkout(self):
+        pass
 
     def logout_user(self, root: tk.Tk, frame: tk.Frame):
         self.destroy_frame(frame)
@@ -53,5 +72,10 @@ class CustomerController(AbstractCustomerController):
         frame.destroy()
 
     @staticmethod
-    def insert_data(tree_view: ttk.Treeview, product_name: str, quantity: int, price: float):
+    def insert_item(tree_view: ttk.Treeview, product_name: str, quantity: int, price: float):
         tree_view.insert('', 'end', text="Item", values=(product_name, quantity, price))
+
+    @staticmethod
+    def remove_item(tree_view: ttk.Treeview):
+        selected_item = tree_view.selection()[0]
+        tree_view.delete(selected_item)
