6c4ac1b12c82f079fe0be3db7d3b28b5e4df43ae
Miracle2001 <47611584+Miracle2001@users.noreply.github.com>
2021-11-25 17:25:51 +0000

    Fixed the returning of orders in the db handler. Fixed type casting of CountryDB and country constructor

--- system/database/db_handler.py
+++ system/database/db_handler.py
@@ -133,7 +133,7 @@ class OrderDB(AbstractOrderDB):
             product_names = order.get_product_names()
             for i in range(len(product_names)):
                 writer.writerow([order.get_customer_name(), product_names[i],
-                                 order.get_order_date(), order.get_subtotal()])
+                                 order.get_order_date(), order.get_order_subtotal()])
 
     def get_customer_orders(self, customer_name: str) -> List:
         with open(self._db_name + ".csv", "r", newline="", encoding="utf-8") as file:
@@ -147,7 +147,7 @@ class OrderDB(AbstractOrderDB):
                     order.append(row[1])
                     order.append(datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S.%f'))
                     order.append(float(row[3]))
-                orders.append(order)
+                    orders.append(order)
             return orders
 
 
@@ -161,7 +161,7 @@ class CountryDB(AbstractCountryDB):
             next(reader)
             for row in reader:
                 if row[1] == country_name:
-                    return Country(row[0], row[1], float(row[2]), float(row[3]))
+                    return Country(int(row[0]), row[1], float(row[2]), float(row[3]))
             return False
 
     def get_all_countries(self) -> List[Country]:
@@ -170,7 +170,6 @@ class CountryDB(AbstractCountryDB):
             next(reader)
             countries = []
             for row in reader:
-                country = Country(row[0], row[1], float(row[2]), float(row[3]))
+                country = Country(int(row[0]), row[1], float(row[2]), float(row[3]))
                 countries.append(country)
             return countries
-
--- system/models/shopping/country.py
+++ system/models/shopping/country.py
@@ -6,28 +6,20 @@ class Country:
     This class represents a model of a country, containing a constructor, and
     the getter methods for its parameters.
     """
-    def __init__(self, country_name: str, country_id: int, vat_percentage: float,
+    def __init__(self, country_id: int, country_name: str, vat_percentage: float,
                  shipping_cost: float):
         """
         This constructor instantiates a country object.
 
-        :param country_name: Name of the country.
         :param country_id: ID of the country.
+        :param country_name: Name of the country.
         :param vat_percentage: VAT percentage of the country.
         :param shipping_cost: Shipping cost of the country.
         """
-        self.country_name = country_name
         self.country_id = country_id
+        self.country_name = country_name
         self.vat_percentage = vat_percentage
         self.shipping_cost = shipping_cost
-
-    def get_country_name(self) -> str:
-        """
-        This method gets the country name.
-
-        :returns: Name of the country.
-        """
-        return self.country_name
 
     def get_country_id(self) -> int:
         """
@@ -36,6 +28,14 @@ class Country:
         :returns: ID of the country.
         """
         return self.country_id
+
+    def get_country_name(self) -> str:
+        """
+        This method gets the country name.
+
+        :returns: Name of the country.
+        """
+        return self.country_name
 
     def get_vat_percentage(self) -> float:
         """
