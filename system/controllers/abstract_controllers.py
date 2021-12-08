4cad6bb3cea8d02849331fb675c52bc0dcceebcf
dylancoffey99 <dylancoffey99@gmail.com>
2021-12-08 06:58:03 +0000

    Added missing abstract methods to abstract_controllers.py

--- system/controllers/abstract_controllers.py
+++ system/controllers/abstract_controllers.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+from typing import List
 from abc import ABC, abstractmethod
 
 
@@ -8,19 +10,19 @@ class AbstractAccessController(ABC):
         pass
 
     @abstractmethod
-    def login_view(self, root: tk.Tk, frame: tk.Frame):
+    def login_view(self, frame: tk.Frame):
         pass
 
     @abstractmethod
-    def register_view(self, root: tk.Tk, frame: tk.Frame):
+    def register_view(self, frame: tk.Frame):
         pass
 
     @abstractmethod
-    def login_user(self, root: tk.Tk, frame: tk.Frame):
+    def login_user(self, frame: tk.Frame):
         pass
 
     @abstractmethod
-    def register_user(self, root: tk.Tk, frame: tk.Frame):
+    def register_user(self, frame: tk.Frame):
         pass
 
     @abstractmethod
@@ -29,6 +31,26 @@ class AbstractAccessController(ABC):
 
 
 class AbstractCustomerController(ABC):
+    @abstractmethod
+    def fill_products(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def add_product(self, tree_view: ttk.Treeview):
+        pass
+
+    @abstractmethod
+    def remove_product(self, tree_view: ttk.Treeview):
+        pass
+
+    @abstractmethod
+    def checkout(self, tree_view: ttk.Treeview):
+        pass
+
+    @abstractmethod
+    def create_order(self, customer_name, product_names):
+        pass
+
     @abstractmethod
     def logout_user(self, root: tk.Tk, frame: tk.Frame):
         pass
@@ -40,9 +62,37 @@ class AbstractCustomerController(ABC):
 
 class AbstractAdminController(ABC):
     @abstractmethod
+    def fill_users(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def fill_products(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def view_order(self, tree_view: ttk.Treeview):
+        pass
+
+    @abstractmethod
+    def add_discount(self, tree_view: ttk.Treeview):
+        pass
+
+    @abstractmethod
+    def add_product(self, tree_view: ttk.Treeview):
+        pass
+
+    @abstractmethod
+    def edit_product(self, tree_view: ttk.Treeview):
+        pass
+
+    @abstractmethod
+    def remove_product(self, tree_view: ttk.Treeview):
+        pass
+
+    @abstractmethod
     def logout_user(self, root: tk.Tk, frame: tk.Frame):
         pass
 
     @abstractmethod
-    def destroy_frame(self,  frame: tk.Frame):
+    def destroy_frame(self, frame: tk.Frame):
         pass
