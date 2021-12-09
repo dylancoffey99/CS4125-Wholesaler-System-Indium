1d11cdf248a977ebeffc5903052c939eb83e43dd
dylancoffey99 <dylancoffey99@gmail.com>
2021-12-09 21:13:22 +0000

    Changed discount_category to discount_id in Customer. Changed its getter/setter also.

--- system/models/users/customer.py
+++ system/models/users/customer.py
@@ -1,15 +1,14 @@
 from system.models.users.user import User
-from system.models.shopping.discount import DiscountCategory
 
 
 class Customer(User):
     def __init__(self, user_name: str, password: str, country_id: int,
-                 discount_category: DiscountCategory = None):
+                 discount_id: int = -1):
         User.__init__(self, user_name, password, 0, country_id)
-        self._discount_category = discount_category
+        self.discount_id = discount_id
 
-    def get_discount_category(self) -> DiscountCategory:
-        return self._discount_category
+    def get_discount_id(self):
+        return self.discount_id
 
-    def set_discount_category(self, discount_category: DiscountCategory):
-        self._discount_category = discount_category
+    def set_discount_id(self, discount_id: int):
+        self.discount_id = discount_id
