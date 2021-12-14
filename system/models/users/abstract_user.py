b64ba5729ed93970cf37e49a95b1ad1fccdb504b
nadinefrawley <18219659@studentmail.ul.ie>
2021-12-14 10:59:32 +0000

    Added Docstrings to the abstract user

--- system/models/users/abstract_user.py
+++ system/models/users/abstract_user.py
@@ -1,19 +1,47 @@
+"""
+This module contains the AbstractUser class.
+"""
 from abc import ABC, abstractmethod
 
 
 class AbstractUser(ABC):
+    """
+    This abstract user class represents an interface for the user. It contains the
+    abstract methods to be implemented into the user access and user database classes.
+    """
+
     @abstractmethod
     def get_user_name(self) -> str:
+        """
+        This method gets a users username.
+
+        :returns: Username of the user.
+        """
         pass
 
     @abstractmethod
     def get_password(self) -> str:
+        """
+        This method gets a users password.
+
+        :returns: Password of the user.
+        """
         pass
 
     @abstractmethod
     def get_is_admin(self) -> int:
+        """
+        This method gets whether a user is admin or customer.
+
+        :returns: Identity of the User.
+        """
         pass
 
     @abstractmethod
     def get_country_id(self) -> int:
+        """
+        This method gets a users country.
+
+        :returns: Country of the user.
+        """
         pass
